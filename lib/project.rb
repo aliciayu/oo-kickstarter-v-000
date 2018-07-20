8b6ecf675d0f19f2c1834b558ba8554bdf5ad446
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-20 03:07:05 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 class Project
-  attr_accessor :title, :backers
+  attr_reader :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-  def add_backer(backers)
-    backers << back_projects
-    backer.backers << self
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
   end
 
 end
