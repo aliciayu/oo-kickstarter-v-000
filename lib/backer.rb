6dccfdebcddefa1159483e66816319b8ce3df633
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-20 02:15:23 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :project
 
   def initialize(name, back_project = {})
     @name = name
-    project = back_project
+    @project = back_project
   end
 
 
