e1dbde65bbf0700b7be778c13b6b68d52f3e2bb2
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-20 02:07:16 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require 'project.rb'
+
+class Backer
+  attr_accessor :name, :back_project
+
+  def initialize(name)
+    @name = name
+    @back_project = []
+  end
+
+  def back_project
+    self.back_project << self
+  end
+  binding.pry
+
+
+end
